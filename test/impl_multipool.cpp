086b78e043a457ecf847aa6807d185a75ada9ba9
Alejandro Seguí <alesegdia@gmail.com>
2015-09-06 21:15:36 +0200

    multipool created; testing started

---
+++ test/impl_multipool.cpp
@@ -0,0 +1,166 @@
+
+#include <chrono>
+#include <iostream>
+#include <new>
+#include <cstdlib>
+#include <cstdio>
+#include <cstdint>
+
+
+template <typename Class, typename T>
+struct Offset {
+	static size_t offset;
+};
+
+template <typename Class, typename T>
+size_t Offset<Class, T>::offset = 0;
+
+template <typename Class, typename... Rest>
+struct ComputeSize;
+
+template <typename Class, typename First, typename... Rest>
+struct ComputeSize<Class, First, Rest...>
+{
+	static size_t getSize() {
+		size_t restsize = ComputeSize<Class, Rest...>::getSize();
+		Offset<Class, First>::offset = restsize;
+		return sizeof(First) + restsize;
+	}
+};
+
+template <typename Class>
+struct ComputeSize<Class> {
+	static size_t getSize() {
+		return 0;
+	}
+};
+
+template <typename Class, typename... Rest>
+struct ChunkAllocator;
+
+template <typename Class, typename First, typename... Rest>
+struct ChunkAllocator<Class, First, Rest...>
+{
+	static void alloc(char* base_ptr) {
+		void* place = base_ptr + Offset<Class, First>::offset;
+		First* obj = new (place) First();
+		ChunkAllocator<Class, Rest...>::alloc(base_ptr);
+	}
+};
+
+template <typename Class>
+struct ChunkAllocator<Class>
+{
+	static void alloc(char* base_ptr) {
+
+	}
+};
+
+
+template <typename SubClass, int NUM_ELEMENTS, typename... Types>
+class MultiPoolFactory
+{
+private:
+	const size_t m_chunkSize = ComputeSize<SubClass, Types...>::getSize();
+	char *m_bytes;
+	size_t m_lastAlloc = 0;
+
+public:
+
+	MultiPoolFactory() {
+		m_bytes = new char[m_chunkSize * NUM_ELEMENTS];
+	}
+
+	~MultiPoolFactory() {
+		delete [] m_bytes;
+	}
+
+	void allocChunk() {
+		ChunkAllocator<SubClass, Types...>::alloc(m_bytes + m_lastAlloc * m_chunkSize);
+		m_lastAlloc++;
+	}
+
+	size_t getChunkSize() {
+		return m_chunkSize;
+	}
+
+	template <typename T>
+	T* getItemForIndex( size_t index ) {
+		char* location = m_bytes + index * m_chunkSize + Offset<SubClass, T>::offset;
+		return ((T*)(location));
+	}
+
+	template <typename T>
+	size_t getOffsetForType() {
+		return Offset<SubClass, T>::offset;
+	}
+
+	template <typename T>
+	size_t getSizeFor() {
+		return sizeof(T);
+	}
+};
+
+
+struct A {
+	std::uint8_t a = 0;
+	std::uint16_t b = 0;
+	std::uint16_t c = 0;
+	std::uint16_t d = 0;
+	std::uint16_t e = 0;
+};
+
+struct B {
+	std::uint16_t a = 0;
+	std::uint32_t b = 0;
+	std::uint32_t c = 0;
+	std::uint32_t d = 0;
+	std::uint32_t e = 0;
+};
+
+struct C {
+	std::uint16_t a;
+	std::uint32_t b;
+};
+
+class TestPool1 : public MultiPoolFactory<TestPool1, 10, A, B, C> {};
+class TestPool2 : public MultiPoolFactory<TestPool2, 10, C, A, B> {};
+
+int main( int argc, char** argv )
+{
+
+	TestPool1 tp1;
+	TestPool2 tp2;
+
+	tp1.allocChunk();
+	tp1.allocChunk();
+	tp1.allocChunk();
+	tp1.allocChunk();
+
+	A* a0 = tp1.getItemForIndex<A>(0);
+	A* a1 = tp1.getItemForIndex<A>(1);
+	A* a2 = tp1.getItemForIndex<A>(2);
+	A* a3 = tp1.getItemForIndex<A>(3);
+
+	B* b0 = tp1.getItemForIndex<B>(0);
+	B* b1 = tp1.getItemForIndex<B>(1);
+	B* b2 = tp1.getItemForIndex<B>(2);
+	B* b3 = tp1.getItemForIndex<B>(3);
+
+
+	/*{
+		std::cout << "Testing MultiPoolFactory\n";
+		std::cout << tp1.getOffsetForType<A>() << std::endl;
+		std::cout << tp1.getOffsetForType<B>() << std::endl;
+		std::cout << tp1.getOffsetForType<C>() << std::endl;
+		std::cout << tp1.getChunkSize() << std::endl;
+
+		std::cout << "Testing MultiPoolFactory\n";
+		std::cout << tp2.getOffsetForType<A>() << std::endl;
+		std::cout << tp2.getOffsetForType<B>() << std::endl;
+		std::cout << tp2.getOffsetForType<C>() << std::endl;
+		std::cout << tp2.getChunkSize() << std::endl;
+	}*/
+
+	return 0;
+}
