5f80101062ae964689584c1d29c7907dc23556ee
Alejandro Seguí <alesegdia@gmail.com>
2025-03-06 21:55:34 +0100

    working on snapshot buffer

--- include/rztl/rztl.h
+++ include/rztl/rztl.h
@@ -7,3 +7,54 @@
 #include "dynamicarray.h"
 #include "unordereddynamicarray.h"
 
+
+
+namespace rztl {
+	template <typename T>
+	class SnapshotBuffer
+	{
+	public:
+		SnapshotBuffer(size_t capacity)
+			: m_capacity(capacity)
+		{
+			m_data = new T[capacity];
+			m_currentTop = 0;
+		}
+
+		~SnapshotBuffer()
+		{
+			delete[] m_data;
+		}
+
+		T& GetElement(int elementIndex)
+		{
+			assert(elementIndex >= 0 && elementIndex < m_capacity);
+			auto translatedIndex = TranslateIndex(elementIndex);
+			assert(translatedIndex >= 0 && translatedIndex < m_capacity);
+			return m_data[translatedIndex];
+		}
+
+		size_t GetCapacity() const
+		{
+			return m_capacity;
+		}
+
+		void Push(const T& elementToAdd)
+		{
+			m_data[m_capacity] = elementToAdd;
+			m_capacity++;
+			m_currentTop = (m_currentTop + 1) % m_capacity;
+		}
+
+	private:
+		int TranslateIndex(int index)
+		{
+			return index + m_currentTop % m_capacity;
+		}
+
+		T* m_data;
+		size_t m_capacity;
+		size_t m_currentTop;
+
+	};
+}
